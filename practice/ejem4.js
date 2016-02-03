62bd021c2b58c0e684e372e6366d9ae5407c3b17
Notysimo <notysimo@gmail.com>
2016-02-02 22:48:24 -0400

    practice function

---
+++ practice/ejem4.js
@@ -0,0 +1,10 @@
+﻿
+/* Created by andres uzeda on 2/2/2016*/
+
+var parOimpar = (function(valor){
+        if(valor%2 == 0){
+                return function() {console.log("es un número par"); } 
+        }else{
+                return function() {console.log("es un número impar");}
+        }
+})
