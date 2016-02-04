4629411b18617178312cfa145d1456e6d8b7aaea
Notysimo <notysimo@gmail.com>
2016-02-03 22:22:33 -0400

    practice function Infynity

---
+++ practice/ejem3.js
@@ -0,0 +1,11 @@
+﻿
+/* Created by andres uzeda on 2/2/2016*/
+
+var i;
+   var max = -Infinity;
+   for(i = 0; i < arguments.length; i++) {
+       if (arguments[i] > max) {
+           max = arguments[i];
+       }
+   }
+   return max;
